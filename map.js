9e218f6f1482169ac8ea956a1eb0a12c310471d0
Victoria Kalenik <vk781@hotmail.com>
2023-08-26 19:02:31 +0000

    End of lesson

--- map.js
+++ map.js
@@ -1,20 +1,32 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node map.js in the terminal
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multiplyByTwo = function (num) {
+  return num * 2;
+}
 
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simplfied = nums.map(function (num) { return num * 2 });
+console.log(simplfied);
 
 // Simplfied w/ map() + arrow function
-
+let arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 // With objects:
 const students = [
@@ -37,3 +49,22 @@ const students = [
     skill: 'CSS'
   },
 ];
+const object = students.map(student => [student.name, student.id]);
+console.log(object);
+
+const keyValue = students.map(student => {
+  return {
+    id: student.id,
+    name: student.name
+  }
+});
+console.log(keyValue);
+
+const withAge = students.map(student => {
+  return {
+    ...student, // Spread the existing properties
+    age: student.id === 1 ? 32 : (student.id === 2 ? 26 : 28) // Add age based on id
+  };
+});
+
+console.log(withAge);
